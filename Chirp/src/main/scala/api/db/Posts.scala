0ea8f83a70271c446660941b7cafa589266d9f24
brzep <brzeppp@gmail.com>
2023-06-20 10:46:20 +0200

    Added Users, Tags and Posts querying functions

--- Chirp/src/main/scala/api/db/Posts.scala
+++ Chirp/src/main/scala/api/db/Posts.scala
@@ -14,25 +14,46 @@ object Posts {
     DbManager.executeRequest(createReactionQuery);
   }
 
-  def createPost(userName: String, postContent: String, tags: List[String]) = {
-    if Users.readUser(userName).isEmpty then throw RuntimeException("Unrecognised user!");
+  def createPost(userName: String, postContent: String, tags: List[String]): String = {
+    if Users.readUser(userName).isEmpty then throw RuntimeException("Unrecognised user!")
 
-    val createPostQuery = s"CREATE (post:POST {content: \"${postContent}\"}) RETURN post;";
-    val response = DbManager.executeRequest(createPostQuery);
+    val createPostQuery = s"CREATE (post:POST {content: \"${postContent}\"}) RETURN post;"
+    val response = DbManager.executeRequest(createPostQuery)
 
     val createdPost = parsePosts(response) match
       case List(post) => post;
       case _else => throw Exception("Failed to created post!");
 
+//    val timestamp: Long = System.currentTimeMillis / 1000
     val createdQuery = s"MATCH (post:POST), (user:USER {name: \"${userName}\"}) " +
       s"WHERE ID(post)=${createdPost.id} " +
       s"CREATE (user)-[r:CREATED]->(post);"
-    DbManager.executeRequest(createdQuery);
+//      s"CREATE (user)-[r:CREATED {datetime: datetime({epochSeconds: ${timestamp})})}]->(post);"
+    DbManager.executeRequest(createdQuery)
 
     tags.foreach(tagName =>
       Tags.createTag(tagName);
       Tags.tagPost(createdPost.id, tagName);
-    );
+    )
+    createdPost.id
+  }
+
+  def commentPost(userName: String, commentContent: String, tags: List[String], postId: String) = {
+    val commentId: String = createPost(userName, commentContent, tags)
+
+    val commentedQuery = s"MATCH (post:POST), (comment:POST) " +
+      s"WHERE ID(post)=${postId} AND ID(comment)=${commentId} " +
+      s"CREATE (comment)-[r:COMMENTED]->(post);"
+    DbManager.executeRequest(commentedQuery)
+  }
+
+  def sharePost(userName: String, postId: String) = {
+//    val timestamp: Long = System.currentTimeMillis / 1000
+    val sharedQuery = s"MATCH (post:POST), (user:USER {name: \"${userName}\"}) " +
+      s"WHERE ID(post)=${postId} " +
+      s"CREATE (user)-[r:SHARED]->(post);"
+    //      s"CREATE (user)-[r:SHARED {datetime: datetime({epochSeconds: ${timestamp})})}]->(post);"
+    DbManager.executeRequest(sharedQuery)
   }
 
   def getAllPosts: List[Post] = {
@@ -45,8 +66,27 @@ object Posts {
     parsePosts(DbManager.executeRequest(getUserPostsQuery))
   }
 
+  def getTagPosts(tagName: String): List[Post] = {
+    val getUserPostsQuery = s"MATCH (post:POST)-[:TAGGED]->(tag:TAG {name: \"${tagName}\"}) RETURN post;"
+    parsePosts(DbManager.executeRequest(getUserPostsQuery))
+  }
+
   def getPostReactions(postId: String): List[ReactedTo] = {
     val getPostReactionsQuery = s"MATCH (user:USER)-[p:REACTED_TO]->(post:POST) WHERE ID(post) = ${postId} RETURN p;"
     parseReactedTo(DbManager.executeRequest(getPostReactionsQuery))
+  }
+
+  def getContentPosts(postContent: String): List[Post] = {
+    getAllPosts.filter(post => post.content.toLowerCase.contains(postContent.toLowerCase))
+  }
+
+  def getSharedPosts(userName: String): List[Post] = {
+    val getSharedPostsQuery = s"MATCH (user:USER {name: \"${userName}\"})-[:SHARED]->(post:POST) RETURN post;"
+    parsePosts(DbManager.executeRequest(getSharedPostsQuery))
+  }
+
+  def getPostSharesQuantity(postId: String): Int = {
+    val getSharesQuery = s"MATCH (user:USER)-[:SHARED]->(post:POST) WHERE ID(post)=${postId} RETURN post;"
+    parsePosts(DbManager.executeRequest(getSharesQuery)).length
   }
 }
