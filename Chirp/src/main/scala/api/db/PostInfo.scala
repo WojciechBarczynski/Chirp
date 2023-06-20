b96a8f75c1b3b63bb5ac764481888ea6c2fb5701
Wojciech Barczynski <barczynski.w@gmail.com>
2023-06-20 09:30:06 +0200

    Add post info

---
+++ Chirp/src/main/scala/api/db/PostInfo.scala
@@ -0,0 +1,16 @@
+package api.db
+
+import objects.ReactionType
+import objects.nodes.parseUsers
+import objects.nodes.Post
+
+class PostInfo(val postId: String, val content: String, val authorName: String, val reactions: Map[String, Int])
+
+def postInfo(post: Post): PostInfo = {
+  val authorNameQuery = s"MATCH (user:USER)-[:CREATED]->(post:POST) WHERE ID(post) = ${post.id} RETURN user;"
+  val authorName = parseUsers(DbManager.executeRequest(authorNameQuery)).last.name;
+  val reactions: List[String] = Posts.getPostReactions(post.id).map(reactedTo => reactedTo.reactionType);
+  val reactionsCount = reactions
+    .foldLeft(Map.empty[String, Int]) {(m: Map[String, Int], x: String) => m + ((x, m.getOrElse(x, 0) + 1))};
+  new PostInfo(post.id, post.content, authorName, reactionsCount)
+}
